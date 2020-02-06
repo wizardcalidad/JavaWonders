942b3b720fa030c1ff7b8d53176db704c6da1c3d
wizardcalidad <mintmassrecruiters@gmail.com>
2020-02-06 12:37:32 +0100

    wonders done with java

--- src/com/cohortThree/Television.java
+++ src/com/cohortThree/Television.java
@@ -1,34 +1,118 @@
 package com.cohortThree;
-
+import java.util.Scanner;
 public class Television {
     private int channelId;
-    private int volume;
+    private int volume = 0;
     private boolean isTvOn;
+    private String channelName = "Apple";
 
+Scanner sc = new Scanner(System.in);
 
-    public void changeChannel(int newChannel){
-        if (isTvOn) {
-            channelId = newChannel;
+    public void turnTvOn(){
+        isTvOn = true;
+        System.out.println("***Life\'s Good***");
+        System.out.printf("You're watching %s%n", channelName);
+    }
+    public boolean isTvOn(){
+        return isTvOn;
+    }
+
+    public void changeChannel() {
+        while (isTvOn == true) {
+            if (isTvOn) {
+                System.out.println("Channel are listed from Number 1 to 10");
+                channelId = sc.nextInt();
+                switch (channelId) {
+                    case 1:
+                        channelName = "Apple";
+                        break;
+                    case 2:
+                        channelName = "Semicolon Africa";
+                        break;
+                    case 3:
+                        channelName = "Mongo DB";
+                        break;
+                    case 4:
+                        channelName = "Code Slayer";
+                        break;
+                    case 5:
+                        channelName = "Intellij";
+                        break;
+                    case 6:
+                        channelName = "Big Decimal";
+                        break;
+                    case 7:
+                        channelName = "Maria DB";
+                        break;
+                    case 8:
+                        channelName = "Nacker Movement";
+                        break;
+                    case 9:
+                        channelName = "Mamba Movement";
+                        break;
+                    case 10:
+                        channelName = "Constructor";
+                        break;
+                    default:
+                        channelName = "AV";
+                        break;
+                }
+                System.out.printf("%s%n%n%n%n%n%n%n", channelName);
+            }
+            else{
+                isTvOn = false;
+            }
         }
+
     }
     public int getChannelId(){
 
         return channelId;
     }
-    public void channelUp(){
-        if (isTvOn) {
-            channelId++;
+    public int channelUp(int channelId){
+        while (isTvOn == true) {
+            if (isTvOn) {
+                channelId++;
+            }
         }
+        return channelId;
     }
-    public void channelDown(){
-        if (isTvOn) {
-            channelId--;
+    public int channelDown(int channelId){
+        while (isTvOn == true) {
+            if (isTvOn) {
+                channelId--;
+            }
         }
+        return channelId;
     }
-    public void turnTvOn(){
-        isTvOn = true;
+
+    public void setVolumeUp(){
+        this.volume = volume + 1;
     }
-    public boolean isTvOn(){
-        return isTvOn;
+
+    public int getVolumeUp(){
+        return volume;
+
     }
+
+//    public void VolumeUp(){
+//       // this.volume =volume;
+//      // while (isTvOn == true) {
+//         //  if (isTvOn) {
+//               volume++;
+//               System.out.printf("Volume: ",volume++);
+//         //  }
+//      // }
+//
+//    }
+//    public void VolumeDown(){
+//       // this.volume =volume;
+//       // while (isTvOn == true) {
+//          //  if (isTvOn) {
+//                volume--;
+//                System.out.printf("Volume: ",volume--);
+//          //  }
+//       // }
+//    }
+
 }
--- src/com/cohortThree/TelevisionTest.java
+++ src/com/cohortThree/TelevisionTest.java
@@ -4,5 +4,14 @@ public class TelevisionTest {
     public static void main(String[] args) {
         Television lg = new Television();
 
+        lg.turnTvOn();
+       lg.changeChannel();
+        lg.channelUp(1);
+        lg.setVolumeUp();
+        lg.setVolumeUp();
+       // lg.VolumeDown();
+
+        System.out.println(lg.getVolumeUp());
+
     }
 }
